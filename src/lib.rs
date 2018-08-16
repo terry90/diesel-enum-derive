123a19bb2d1a486c9eecdb74d97e9e81205df672
Terry Raimondo <terry.raimondo@gmail.com>
2018-08-16 18:39:40 +0200

    Add Nullable support + snake case literals

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,10 @@ extern crate proc_macro;
 extern crate syn;
 #[macro_use]
 extern crate quote;
+extern crate heck;
 extern crate proc_macro2;
 
+use heck::SnakeCase;
 use proc_macro::TokenStream;
 use proc_macro2::{Literal, Span};
 use syn::Ident;
@@ -25,7 +27,7 @@ pub fn diesel_enum(input: TokenStream) -> TokenStream {
         let variants_literal = enum_data
             .variants
             .iter()
-            .map(|vs| vs.ident.to_string().to_lowercase())
+            .map(|vs| vs.ident.to_string().to_snake_case())
             .collect::<Vec<_>>();
 
         impl_diesel_enum(name, &variants, &variants_literal)
@@ -44,81 +46,100 @@ fn impl_diesel_enum(
     let name_iter2 = std::iter::repeat(&name);
     let name_iter3 = std::iter::repeat(&name);
 
+    let scope = Ident::new(&format!("diesel_enum_{}", name), Span::call_site());
+
     let bytes_literal = &variants_literal
         .iter()
         .map(|vl| Literal::byte_string(vl.as_bytes()))
         .collect::<Vec<_>>();
 
     let expanded = quote! {
-        use diesel::deserialize::{self, FromSql, FromSqlRow, Queryable};
-        use diesel::dsl::AsExprOf;
-        use diesel::expression::AsExpression;
-        use diesel::pg::Pg;
-        use diesel::row::Row;
-        use diesel::serialize::{self, IsNull, Output, ToSql};
-        use diesel::sql_types::VarChar;
-        use std::error::Error;
-        use std::fmt;
-        use std::io::Write;
+        mod #scope {
+            use super::*;
+            use diesel::deserialize::{self, FromSql, FromSqlRow, Queryable};
+            use diesel::dsl::AsExprOf;
+            use diesel::expression::AsExpression;
+            use diesel::pg::Pg;
+            use diesel::row::Row;
+            use diesel::serialize::{self, IsNull, Output, ToSql};
+            use diesel::sql_types::{VarChar, Nullable};
+            use std::error::Error;
+            use std::fmt;
+            use std::io::Write;
 
-        impl fmt::Display for #name {
-            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                write!(
-                    f,
-                    "{}",
+            impl fmt::Display for #name {
+                fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                    write!(
+                        f,
+                        "{}",
+                        match *self {
+                            #(#name_iter::#variants => #variants_literal,)*
+                        }
+                    )
+                }
+            }
+
+            impl AsExpression<VarChar> for #name {
+                type Expression = AsExprOf<String, VarChar>;
+                fn as_expression(self) -> Self::Expression {
+                    <String as AsExpression<VarChar>>::as_expression(self.to_string())
+                }
+            }
+
+            impl<'a> AsExpression<VarChar> for &'a #name {
+                type Expression = AsExprOf<String, VarChar>;
+                fn as_expression(self) -> Self::Expression {
+                    <String as AsExpression<VarChar>>::as_expression(self.to_string())
+                }
+            }
+
+            impl AsExpression<Nullable<VarChar>> for #name {
+                type Expression = AsExprOf<String, Nullable<VarChar>>;
+                fn as_expression(self) -> Self::Expression {
+                    <String as AsExpression<Nullable<VarChar>>>::as_expression(self.to_string())
+                }
+            }
+
+            impl<'a> AsExpression<Nullable<VarChar>> for &'a #name {
+                type Expression = AsExprOf<String, Nullable<VarChar>>;
+                fn as_expression(self) -> Self::Expression {
+                    <String as AsExpression<Nullable<VarChar>>>::as_expression(self.to_string())
+                }
+            }
+
+            impl ToSql<VarChar, Pg> for #name {
+                fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
                     match *self {
-                        #(#name_iter::#variants => #variants_literal,)*
+                        #(#name_iter1::#variants => out.write_all(#bytes_literal)?,)*
                     }
-                )
-            }
-        }
-
-        impl AsExpression<VarChar> for #name {
-            type Expression = AsExprOf<String, VarChar>;
-            fn as_expression(self) -> Self::Expression {
-                <String as AsExpression<VarChar>>::as_expression(self.to_string())
-            }
-        }
-
-        impl<'a> AsExpression<VarChar> for &'a #name {
-            type Expression = AsExprOf<String, VarChar>;
-            fn as_expression(self) -> Self::Expression {
-                <String as AsExpression<VarChar>>::as_expression(self.to_string())
-            }
-        }
-
-        impl ToSql<VarChar, Pg> for #name {
-            fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
-                match *self {
-                    #(#name_iter1::#variants => out.write_all(#bytes_literal)?,)*
-                }
-                Ok(IsNull::No)
-            }
-        }
-
-        impl FromSql<VarChar, Pg> for #name {
-            fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
-                match not_none!(bytes) {
-                    #(#bytes_literal => Ok(#name_iter2::#variants),)*
-                    v => Err(format!("Unknown value {:?} for {}", v, stringify!(#name)).into()),
+                    Ok(IsNull::No)
                 }
             }
-        }
 
-        impl FromSqlRow<VarChar, Pg> for #name {
-            fn build_from_row<R: Row<Pg>>(row: &mut R) -> Result<Self, Box<Error + Send + Sync>> {
-                match String::build_from_row(row)?.as_ref() {
-                    #(#variants_literal => Ok(#name_iter3::#variants),)*
-                    v => Err(format!("Unknown value {} for {}", v, stringify!(#name)).into()),
+            impl FromSql<VarChar, Pg> for #name {
+                fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
+                    match not_none!(bytes) {
+                        #(#bytes_literal => Ok(#name_iter2::#variants),)*
+                        v => Err(format!("Unknown value {:?} for {}", v, stringify!(#name)).into()),
+                    }
                 }
             }
-        }
 
-        impl Queryable<VarChar, Pg> for #name {
-            type Row = Self;
+            impl FromSqlRow<VarChar, Pg> for #name {
+                fn build_from_row<R: Row<Pg>>(row: &mut R) -> Result<Self, Box<Error + Send + Sync>> {
+                    match String::build_from_row(row)?.as_ref() {
+                        #(#variants_literal => Ok(#name_iter3::#variants),)*
+                        v => Err(format!("Unknown value {} for {}", v, stringify!(#name)).into()),
+                    }
+                }
+            }
 
-            fn build(row: Self::Row) -> Self {
-                row
+            impl Queryable<VarChar, Pg> for #name {
+                type Row = Self;
+
+                fn build(row: Self::Row) -> Self {
+                    row
+                }
             }
         }
     };
